38589a9314e1b1e1dabbf6c1c1da8def46a0fb0f
JGeli <jaybee.aicpa@gmail.com>
2022-04-12 17:25:04 +0800

    first commit

---
+++ screens/Authentication/SignIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+import { AuthLayout } from "../"
+
+const SignIn = () => {
+    return (
+        <AuthLayout
+            title="Let's Sign You In"
+        >
+            
+        </AuthLayout>
+    )
+}
+
+export default SignIn;
